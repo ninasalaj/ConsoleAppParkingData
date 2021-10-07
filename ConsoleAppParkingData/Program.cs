a5b1d5986215fa77f8f1c407caf4dfd1e8a2fb67
Ivan Špeh <ivan.speh@gmail.com>
2021-10-07 20:51:45 +0200

    GetSorteredCities method added

--- ConsoleAppParkingData/Program.cs
+++ ConsoleAppParkingData/Program.cs
@@ -20,16 +20,17 @@ namespace ConsoleAppParkingData
             Console.WriteLine("4 - ispis gradova abecedno silazno");
             var option = Convert.ToInt32(Console.ReadLine());
 
+            var cities = new List<ParkingCity>();
             switch(option)
             {
                 case 1:
                     Console.WriteLine("Unesite naziv grada");
                     var searchString = Console.ReadLine();
 
-                    var city = parkingCities.Where(x => x.Name == searchString).FirstOrDefault();
-                    if(city == null)
-                        Console.WriteLine("Grad nije pronađen");
-                    else Console.WriteLine(city.Name);
+                    var city = parkingCities.Where(x => x.Name.Contains(searchString)).ToList();
+                    //if(city == null)
+                    //    Console.WriteLine("Grad nije pronađen");
+                    //else Console.WriteLine(city.Name);
                 break;
                 case 2:
                     foreach (var parkingCity in parkingCities) 
@@ -39,15 +40,12 @@ namespace ConsoleAppParkingData
 
                     break;
                 case 3:
-                    var cities1 = parkingCities.OrderBy(x=>x.Name);
-                    foreach (var parkingCity in cities1)
-                    {
-                        Console.WriteLine(parkingCity.Name);
-                    }
+                    cities = GetSorteredCities(parkingCities);
+
                     break;
                 case 4:
-                    var cities2 = parkingCities.OrderByDescending(x => x.Name);
-                    foreach (var parkingCity in cities2)
+                    cities = parkingCities.OrderByDescending(x => x.Name).ToList();
+                    foreach (var parkingCity in cities)
                     {
                         Console.WriteLine(parkingCity.Name);
                     }
@@ -55,23 +53,23 @@ namespace ConsoleAppParkingData
             }
             Console.WriteLine();
             var maxZonesCity = parkingCities.OrderByDescending(x => x.Zones.Count).First();
-            Console.WriteLine("Grad s najvise zona je " + maxZonesCity.Name);
+            Console.WriteLine($"Grad s najvise zona je {maxZonesCity.Name}");
             Console.WriteLine();
             Console.WriteLine("Ispis gradova po broju zona uzlazno: ");
-            var cities3 = parkingCities.OrderBy(x => x.Zones.Count);
-            foreach(var parkingCity in cities3)
+            cities = parkingCities.OrderBy(x => x.Zones.Count).ToList();
+            foreach(var parkingCity in cities)
             {
                 Console.WriteLine(parkingCity.Name);
             }
             Console.WriteLine();
             Console.WriteLine("Unesite naziv zone");
             var searchZone = Console.ReadLine();
-            var cities4 = parkingCities.Where(s => s.Zones.Any(x =>x.Name==searchZone)).ToList();
-            if (cities4.Count == 0)
+            cities = parkingCities.Where(s => s.Zones.Any(x =>x.Name==searchZone)).ToList();
+            if (cities.Count == 0)
                 Console.WriteLine("Gradovi s tom zonom ne postoje.");
             else
             {
-                foreach (var city in cities4)
+                foreach (var city in cities)
                 {
                     Console.WriteLine(city.Name);
                 }
@@ -79,8 +77,8 @@ namespace ConsoleAppParkingData
             }
             Console.WriteLine();
             Console.WriteLine("Gradovi koji počinju sa slovom V i imaju više od 2 zone:");
-            var cities5 = parkingCities.Where(x => x.Name[0] == 'V' & x.Zones.Count > 2).ToList();
-            foreach (var city in cities5)
+            cities = parkingCities.Where(x => x.Name[0] == 'V' && x.Zones.Count > 2).ToList();
+            foreach (var city in cities)
             {
                 Console.WriteLine(city.Name);
             }
@@ -93,6 +91,17 @@ namespace ConsoleAppParkingData
             }*/
 
 
+        }
+
+        public static List<ParkingCity> GetSorteredCities(List<ParkingCity> parkingCities)
+        {
+            var cities = parkingCities.OrderBy(x => x.Name).ToList();
+            //foreach (var parkingCity in cities)
+            //{
+            //    Console.WriteLine(parkingCity.Name);
+            //}
+
+            return cities;
         }
 
     }
