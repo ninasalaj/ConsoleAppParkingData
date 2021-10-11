eaf3c4b636c33617ca7e9aae3088925a35ce617a
Nina Salaj <nina.salaj.ns@gmail.com>
2021-10-11 18:53:30 +0200

    Code refactoring

--- ConsoleAppParkingData/Program.cs
+++ ConsoleAppParkingData/Program.cs
@@ -14,10 +14,13 @@ namespace ConsoleAppParkingData
             var parkingCities = Newtonsoft.Json.JsonConvert.DeserializeObject<List<ParkingCity>>(json);
 
             Console.WriteLine("Odaberite opciju");
-            Console.WriteLine("1 - pretraga");
-            Console.WriteLine("2 - ispis gradova");
+            Console.WriteLine("1 - pretraga grada");
+            Console.WriteLine("2 - ispis svih gradova");
             Console.WriteLine("3 - ispis gradova abecedno uzlazno");
             Console.WriteLine("4 - ispis gradova abecedno silazno");
+            Console.WriteLine("5 - ispis gradova po broju zona uzlazno");
+            Console.WriteLine("6 - pretraga zone");
+            Console.WriteLine("7 - ispis gradova koji pocinju sa \"V\" i imaju vise od dvije zone");
             var option = Convert.ToInt32(Console.ReadLine());
 
             var cities = new List<ParkingCity>();
@@ -26,47 +29,83 @@ namespace ConsoleAppParkingData
                 case 1:
                     Console.WriteLine("Unesite naziv grada");
                     var searchString = Console.ReadLine();
-
-                    var city = parkingCities.Where(x => x.Name.Contains(searchString)).ToList();
-                    //if(city == null)
-                    //    Console.WriteLine("Grad nije pronađen");
-                    //else Console.WriteLine(city.Name);
+                    cities = GetSearchedCities(parkingCities, searchString);
+                    CitiesOutput(cities);
                 break;
                 case 2:
                     foreach (var parkingCity in parkingCities) 
                     {
-                        Console.WriteLine("{0}, companyId: {1}, broj zona: {2} ", parkingCity.Name, parkingCity.CompanyId, parkingCity.Zones.Count);
+                        Console.WriteLine($"{parkingCity.Name}, companyId: {parkingCity.CompanyId}, broj zona: {parkingCity.Zones.Count} ");
                     }
 
                     break;
                 case 3:
                     cities = GetSorteredCities(parkingCities);
-
+                    CitiesOutput(cities);
                     break;
                 case 4:
-                    cities = parkingCities.OrderByDescending(x => x.Name).ToList();
-                    foreach (var parkingCity in cities)
-                    {
-                        Console.WriteLine(parkingCity.Name);
-                    }
+                    cities = GetDescendinglySorteredCities(parkingCities);
+                    CitiesOutput(cities);
+                    break;
+                case 5:
+                    cities = GetSorteredCitiesByNumberOfZones(parkingCities);
+                    CitiesOutput(cities);
+                    break;
+                case 6:
+                    Console.WriteLine("Unesite naziv zone");
+                    var searchZone = Console.ReadLine();
+                    cities = GetCitiesSearchedByZone(parkingCities, searchZone);
+                    CitiesOutput(cities);
+                    break;
+                case 7:
+                    cities = GetFilteredCities(parkingCities);
+                    CitiesOutput(cities);
+                    break;
+                default:
+                    Console.WriteLine("Opcija ne postoji");
                     break;
             }
-            Console.WriteLine();
-            var maxZonesCity = parkingCities.OrderByDescending(x => x.Zones.Count).First();
-            Console.WriteLine($"Grad s najvise zona je {maxZonesCity.Name}");
-            Console.WriteLine();
-            Console.WriteLine("Ispis gradova po broju zona uzlazno: ");
-            cities = parkingCities.OrderBy(x => x.Zones.Count).ToList();
-            foreach(var parkingCity in cities)
-            {
-                Console.WriteLine(parkingCity.Name);
-            }
-            Console.WriteLine();
-            Console.WriteLine("Unesite naziv zone");
-            var searchZone = Console.ReadLine();
-            cities = parkingCities.Where(s => s.Zones.Any(x =>x.Name==searchZone)).ToList();
+            
+            
+        }
+        public static List<ParkingCity> GetSearchedCities(List<ParkingCity> parkingCities,string searchString)
+        {
+            var cities = parkingCities.Where(x => x.Name.Contains(searchString)).ToList();
+            return cities;
+        }
+
+        public static List<ParkingCity> GetSorteredCities(List<ParkingCity> parkingCities)
+        {
+            var cities = parkingCities.OrderBy(x => x.Name).ToList();
+            return cities;
+        }
+
+        public static List<ParkingCity> GetDescendinglySorteredCities(List<ParkingCity> parkingCities)
+        {
+            var cities = parkingCities.OrderByDescending(x => x.Name).ToList();
+            return cities;
+        }
+
+        public static List<ParkingCity> GetSorteredCitiesByNumberOfZones(List<ParkingCity> parkingCities)
+        {
+            var cities = parkingCities.OrderBy(x => x.Zones.Count).ToList();
+            return cities;
+        }
+
+        public static List<ParkingCity> GetCitiesSearchedByZone(List<ParkingCity> parkingCities, string searchZone)
+        {
+            var cities = parkingCities.Where(s => s.Zones.Any(x => x.Name == searchZone)).ToList();
+            return cities;
+        }
+        public static List<ParkingCity> GetFilteredCities(List<ParkingCity> parkingCities)
+        {
+            var cities = parkingCities.Where(x => x.Name[0] == 'V' && x.Zones.Count > 2).ToList();
+            return cities;
+        }
+        public static void CitiesOutput(List<ParkingCity> cities)
+        {
             if (cities.Count == 0)
-                Console.WriteLine("Gradovi s tom zonom ne postoje.");
+                Console.WriteLine("Ne postoji.");
             else
             {
                 foreach (var city in cities)
@@ -75,34 +114,7 @@ namespace ConsoleAppParkingData
                 }
                 Console.WriteLine();
             }
-            Console.WriteLine();
-            Console.WriteLine("Gradovi koji počinju sa slovom V i imaju više od 2 zone:");
-            cities = parkingCities.Where(x => x.Name[0] == 'V' && x.Zones.Count > 2).ToList();
-            foreach (var city in cities)
-            {
-                Console.WriteLine(city.Name);
-            }
-
-         
-            /*
-            foreach (var parkingCity in parkingCities)
-            {
-                Console.WriteLine(parkingCity.Name);
-            }*/
-
 
         }
-
-        public static List<ParkingCity> GetSorteredCities(List<ParkingCity> parkingCities)
-        {
-            var cities = parkingCities.OrderBy(x => x.Name).ToList();
-            //foreach (var parkingCity in cities)
-            //{
-            //    Console.WriteLine(parkingCity.Name);
-            //}
-
-            return cities;
-        }
-
     }
 }
